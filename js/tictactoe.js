ef3bfddad775806dbc56b82a439fad76c0c3e896
nicolasjhampton <nicolasjhampton@gmail.com>
2016-10-27 16:18:49 -0700

    working on minimax algorithm

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -10,32 +10,66 @@ function TicTacToe() {
   ];
 }
 
+/**
+ * Tests a space for the presence of a player
+ *
+ * @param {Integer} player either 1 = O or -1 = X
+ * @param {Array} board either one or two dimensional square array
+ * @param {Integer} y y coordinate (y, x)
+ * @param {Integer} x x coordinate (y, x)
+ * @returns {boolean} hasWon Is the player in the space?
+ */
+var getSpace = function(board, y, x) {
+  return board[y][x];
+};
 
 /**
  * Tests a space for the presence of a player
  *
- * @param player: Integer - either 1 = O or -1 = X
- *        board: Array - either one or two dimensional square array
- *        coordinates: Integers - (y, x)
- *
- * @returns boolean: Is the player in the space?
- *
+ * @param {Integer} player either 1 = O or -1 = X
+ * @param {Array} board either one or two dimensional square array
+ * @param {Integer} y y coordinate (y, x)
+ * @param {Integer} x x coordinate (y, x)
+ * @returns {boolean} hasWon Is the player in the space?
  */
 var testSpace = function(player, board, y, x) {
-  return player == board[y][x];
+  return player == getSpace(board, y, x);
 };
 
 
 /**
+ * Recursively adds a line that runs across arrays for a checksum
+ * Basically, we're lining up each getSpace call in a line of sumation getSpace
+ *
+ * @example return getSpace(first) + getSpace(second) + getSpace(third);
+ *
+ * @param {Array} board either one or two dimensional square array
+ * @param {Integer} x starting x coordinate
+ * @param {Integer} xstep direction of movement (1 = positive, -1 = negative, 0 = stationary)
+ * @param {Integer} y starting x coordinate
+ * @param {Integer} ystep direction of movement (1 = positive, -1 = negative, 0 = stationary)
+ * @returns {boolean} isWon true = win, false = noWin
+ */
+var getLine = function(board, y, yStep, x, xStep) {
+  return (board.length > y + 1) ?
+    getSpace(board, y, x) + getLine(board, y + yStep, yStep, x + xStep, xStep) :
+    getSpace(board, y, x);
+}
+
+
+/**
  * Recursively tests a line that runs across arrays for a win
+ * Basically, we're lining up each testSpace call in a line of boolean testSpace
  *
- * @param board: Array - either one or two dimensional square array
- *        player: Integer - either 1 = O or -1 = X
- *        y and x: Integers - starting y and x coordinates
- *        ystep: Integer - direction of movement
- *               (1 = positive, -1 = negative, 0 = stationary)
+ * @example return testSpace(first) && testSpace(second) && testSpace(third);
  *
- * @returns boolean: true = win, false = noWin
+ * @param {Array} board either one or two dimensional square array
+ * @param {Integer} player 1 = O or -1 = X
+ * @param {Integer} x starting x coordinate
+ * @param {Integer} xstep direction of movement (1 = positive, -1 = negative, 0 = stationary)
+ * @param {Integer} y starting x coordinate
+ * @param {Integer} ystep direction of movement (1 = positive, -1 = negative, 0 = stationary)
+ * @returns {boolean} isWon true = win, false = noWin
  *
  */
 var testLine = function(board, player, y, yStep, x, xStep) {
@@ -48,9 +82,7 @@ var testLine = function(board, player, y, yStep, x, xStep) {
 /**
  * Tests the game for win status
  *
- * @returns boolean: Does the game continue? Returns
- *                   false for a win. Null is draw.
- *
+ * @returns {Boolean} hasWon Does the game continue? Null is draw.
  */
 TicTacToe.prototype.checkWin = function() {
   var that = this;
@@ -83,15 +115,40 @@ TicTacToe.prototype.checkWin = function() {
   return noWin;
 };
 
+/**
+ * Gets the checksum array for a minimax calculation
+ *
+ * @returns {Array} checksumArray Does the game continue? Null is draw.
+ */
+TicTacToe.prototype.getSums = function() {
+
+  // [sum, sum, sum]
+  var horizontal = this.board.map(function(current, index, self) {
+    return getLine(self, index, 0, 0, 1);
+  });
+
+  // [sum, sum, sum]
+  var vertical = this.board.map(function(current, index, self) {
+    return getLine(self, 0, 1, index, 0);
+  });
+
+  // returns single sum
+  var diagonal1 = getLine(this.board, 0, 1, 0, 1);
+
+  // returns single sum
+  var diagonal2 = getLine(this.board, 0, 1, this.board.length - 1, -1);
+
+  return horizontal.concat(vertical).concat(diagonal1).concat(diagonal2);
+}
+
 
 /**
  * Makes a game move
  *
- * @param  x: Integer - X coordinate in grid system
- *         y: Integer - Y coordinate in grid system
+ * @param {Integer} x X coordinate in grid system
+ * @param {Integer} y Y coordinate in grid system
  *
- * @returns Boolean: Is another move allowed?
- *          False returned for win, null for a draw
+ * @returns {Boolean} isWon Is another move allowed? null for a draw
  *
  */
 TicTacToe.prototype.move = function(x, y) {
@@ -105,9 +162,9 @@ TicTacToe.prototype.move = function(x, y) {
  * Translates the DOM element's index in the jquery array
  * into a coordinate position in the game logic
  *
- * @param Integer index of jquery's spaces array
+ * @param {Integer} Integer index of jquery's spaces array
  *
- * @returns coordinate object { x, y }
+ * @returns {Object} coordinate object { x, y }
  *
  */
  TicTacToe.prototype.calcPosition = function(index) {
